904aa5a1d8cd34c01c2a3a692b73bcd3d4101731
pkhyun <pkh954533@gmail.com>
2024-06-04 12:29:56 +0900

    init commit

---
+++ src/main/java/com/sparta/reviewspotproject/ReviewSpotProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.reviewspotproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewSpotProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewSpotProjectApplication.class, args);
+    }
+
+}
