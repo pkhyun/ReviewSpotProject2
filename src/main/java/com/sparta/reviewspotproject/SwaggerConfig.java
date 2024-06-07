a21bb845b94bad9c968d526aa85c747edbd4cc04
pkhyun <pkh954533@gmail.com>
2024-06-07 11:45:24 +0900

    스웨거 토큰 기능 추가

--- src/main/java/com/sparta/reviewspotproject/SwaggerConfig.java
+++ src/main/java/com/sparta/reviewspotproject/SwaggerConfig.java
@@ -3,15 +3,43 @@ package com.sparta.reviewspotproject;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
+        // 액세스 토큰 보안 스키마 설정
+        SecurityScheme accessTokenScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // 리프레시 토큰 보안 스키마 설정
+        SecurityScheme refreshTokenScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Refresh-Token");
+
+        // 보안 요구 사항 설정
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("accessTokenAuth")
+                .addList("refreshTokenAuth");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes("accessTokenAuth", accessTokenScheme)
+                        .addSecuritySchemes("refreshTokenAuth", refreshTokenScheme))
+                .security(Arrays.asList(securityRequirement))
                 .info(apiInfo());
     }
 
